8a65ac949acc5cd6467c1a94126eb96ce85d2f05
Rafael <rafa14367soliveira@gmail.com>
2023-10-13 20:46:02 -0300

    Adicionar arquivos de projeto.

---
+++ UniversityProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityProject.Models;
+
+namespace UniversityProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
