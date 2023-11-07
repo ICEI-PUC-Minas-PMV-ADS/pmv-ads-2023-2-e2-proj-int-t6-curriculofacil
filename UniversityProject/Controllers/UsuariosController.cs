b7caf0655249566fe43225429402d1ebbd04cf9e
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2023-11-07 15:07:15 -0300

    adiciona Microsoft.AspNetCore.Authorization; ao projeto

--- UniversityProject/Controllers/UsuariosController.cs
+++ UniversityProject/Controllers/UsuariosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
