dcb5a6fb6bcab9788243e73017037533a64fae39
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2023-11-07 15:22:24 -0300

    Adiciona display name ao Model

--- UniversityProject/Models/Curriculo.cs
+++ UniversityProject/Models/Curriculo.cs
@@ -8,28 +8,35 @@ namespace UniversityProject.Models
     {
         [Key]
         public int CVID { get; set; }
-
+        [Display(Name ="Nome")]
         [Required(ErrorMessage = "Obrigatorio informar o Nome")]  
         public string FirstName { get; set; }
 
+        [Display(Name = "Sobrenome")]
         [Required(ErrorMessage = "Obrigatorio informar o Sobreome")]
         public string LastName { get; set; }
 
+        [Display(Name = "Telefone")]
         [Required(ErrorMessage = "Obrigatorio informar o Telefone")]
         public int PhoneNumber { get; set; }
 
+        [Display(Name = "Endereço")]
         [Required(ErrorMessage = "Obrigatorio informar o Endereço")]
         public string Address { get; set; }
 
+        [Display(Name = "Cidade")]
         [Required(ErrorMessage = "Obrigatorio informar o Cidade")]
         public string City { get; set; }
 
+        [Display(Name = "Cep")]
         [Required(ErrorMessage = "Obrigatorio informar o Cep")]
         public int PostalCode { get; set; }
 
+        [Display(Name = "Estado")]
         [Required(ErrorMessage = "Obrigatorio informar o Estado")]
         public string Estate { get; set; }
 
+        [Display(Name = "Objetivo Profissional")]
         [Required(ErrorMessage = "Obrigatorio informar o Objetivo")]
         public string Objetive { get; set; }
    
