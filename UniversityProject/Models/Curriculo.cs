1ea8cb65963c9f69d82fd61f393765efea4e9d86
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2023-11-08 11:46:42 -0300

    Revert "alterações no models curriculo para padrão do projeto"
    This reverts commit 1675c75d0bd00262e5fa1c8eb9cb8b36d727bf9d.

--- UniversityProject/Models/Curriculo.cs
+++ UniversityProject/Models/Curriculo.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Policy;
 
 namespace UniversityProject.Models
@@ -9,34 +8,13 @@ namespace UniversityProject.Models
         [Key]
         public int CVID { get; set; }
 
-        [Required(ErrorMessage = "Obrigatorio informar o Nome")]  
         public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Obrigatorio informar o Sobreome")]
         public string LastName { get; set; }
-
-        [Required(ErrorMessage = "Obrigatorio informar o Telefone")]
-        public int PhoneNumber { get; set; }
-
-        [Required(ErrorMessage = "Obrigatorio informar o Endereço")]
+        public string PhoneNumber { get; set; }
         public string Address { get; set; }
+        public string Email { get; set; }
 
-        [Required(ErrorMessage = "Obrigatorio informar o Cidade")]
-        public string City { get; set; }
-
-        [Required(ErrorMessage = "Obrigatorio informar o Cep")]
-        public int PostalCode { get; set; }
-
-        [Required(ErrorMessage = "Obrigatorio informar o Estado")]
-        public string Estate { get; set; }
-
-        [Required(ErrorMessage = "Obrigatorio informar o Objetivo")]
-        public string Objetive { get; set; }
-   
-
-        [ForeignKey("IdUsuario")]
-        private int IdUsuario { get; set; }
-
-        
+        public int ID { get; set; }
+        public Usuario Usuario { get; set; }
     }
 }
