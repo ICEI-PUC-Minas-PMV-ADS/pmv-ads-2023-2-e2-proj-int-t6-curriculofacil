d07a824ca707acb62e857b7ecf1d2e5c2fec44de
Philippe Mesquita <philippermesquita@gmail.com>
2023-11-14 12:54:23 -0300

    Correção do display name

--- UniversityProject/Models/Curriculo.cs
+++ UniversityProject/Models/Curriculo.cs
@@ -10,35 +10,35 @@ namespace UniversityProject.Models
         [Key]
         public int CurriculoID { get; set; }
         [Display(Name = "Nome")]
-        [Required(ErrorMessage = "Obrigatorio informar o Nome")]
+        [Required(ErrorMessage = "Obrigatório informar o Nome")]
         public string FirstName { get; set; }
 
         [Display(Name = "Sobrenome")]
-        [Required(ErrorMessage = "Obrigatorio informar o Sobreome")]
+        [Required(ErrorMessage = "Obrigatório informar o Sobreome")]
         public string LastName { get; set; }
 
         [Display(Name = "Telefone")]
-        [Required(ErrorMessage = "Obrigatorio informar o Telefone")]
+        [Required(ErrorMessage = "Obrigatório informar o Telefone")]
         public string PhoneNumber { get; set; }
 
         [Display(Name = "Endereço")]
-        [Required(ErrorMessage = "Obrigatorio informar o Endereço")]
+        [Required(ErrorMessage = "Obrigatório informar o Endereço")]
         public string Address { get; set; }
 
         [Display(Name = "Cidade")]
-        [Required(ErrorMessage = "Obrigatorio informar o Cidade")]
+        [Required(ErrorMessage = "Obrigatório informar o Cidade")]
         public string City { get; set; }
 
-        [Display(Name = "Cep")]
-        [Required(ErrorMessage = "Obrigatorio informar o Cep")]
+        [Display(Name = "CEP")]
+        [Required(ErrorMessage = "Obrigatório informar o CEP")]
         public string PostalCode { get; set; }
 
         [Display(Name = "Estado")]
-        [Required(ErrorMessage = "Obrigatorio informar o Estado")]
+        [Required(ErrorMessage = "Obrigatório informar o Estado")]
         public string Estate { get; set; }
 
         [Display(Name = "Objetivo Profissional")]
-        [Required(ErrorMessage = "Obrigatorio informar o Objetivo")]
+        [Required(ErrorMessage = "Obrigatório informar o Objetivo Profissional")]
         public string Objetive { get; set; }
 
         [ForeignKey("UsuarioId")]
