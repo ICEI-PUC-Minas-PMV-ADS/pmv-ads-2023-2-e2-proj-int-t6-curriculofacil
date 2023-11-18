ae053198cba8eca277a096716882270d3d42087c
Rafael <rafa14367soliveira@gmail.com>
2023-11-18 09:37:26 -0300

    Criação do model de historico profissional.

--- UniversityProject/Migrations/20231118123704_HistoricoProfissional.Designer.cs
+++ UniversityProject/Migrations/20231118123704_HistoricoProfissional.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UniversityProject.Data;
 
@@ -10,9 +11,11 @@ using UniversityProject.Data;
 namespace UniversityProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231118123704_HistoricoProfissional")]
+    partial class HistoricoProfissional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UniversityProject/Models/HistoricoProfissional.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniversityProject.Models
+{
+    public class HistoricoProfissional
+    {
+
+        [Key]
+        public int HistoricoProfissinalID { get; set; }
+
+        [Display(Name = "Cargo")]
+        public string Cargo { get; set; }
+        [Display(Name = "Empregador")]
+        public string Empregador { get; set; }
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+        [Display(Name = "Data de inicio")]
+        public DateTime DataInicio { get; set; }
+        [Display(Name = "Data de termino")]
+        public DateTime DataTermino { get; set; }
+
+        [ForeignKey("CurriculoID")]
+        public int CurriculoID { get; set; }
+
+        public Curriculo? Curriculo { get; set; }
+
+    }
+}
+
