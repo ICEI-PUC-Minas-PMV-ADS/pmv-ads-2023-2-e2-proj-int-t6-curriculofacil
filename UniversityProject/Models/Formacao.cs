cdf64b42e5a1c2b3c83bc855967eccb071270943
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2023-11-19 11:28:54 -0300

    adiciona error mensage ao projeto

--- UniversityProject/Models/Formacao.cs
+++ UniversityProject/Models/Formacao.cs
@@ -8,17 +8,24 @@ namespace UniversityProject.Models
         [Key]
         public int FormacaoID { get; set; }
         [Display(Name = "Instituição de ensino ")]
+        [Required(ErrorMessage = "Obrigatorio informar a Instituição de Ensino")]
         public string InstDeEnsino { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar o Estado")]
         [Display(Name = "Estado")]
         public string Estado { get; set; }
         [Display(Name = "Diploma")]
+        [Required(ErrorMessage = "Obrigatorio informar o Diploma")]
         public string Diploma { get; set; }
         [Display(Name = "Campo de estudo")]
+        [Required(ErrorMessage = "Obrigatorio informar o Campo de estudo")]
         public string CampoDeEstudo { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar o Situação")]
         public string Situacao { get; set; }
         [Display(Name = "Data de inicio")]
+        [Required(ErrorMessage = "Obrigatorio informar a Data de Inicio")]
         public DateTime DataInicio { get; set; }
         [Display(Name = "Data de termino")]
+        [Required(ErrorMessage = "Obrigatorio informar a Data de termino")]
         public DateTime DataTermino { get; set; }
 
         [ForeignKey("CurriculoID")]
--- UniversityProject/Models/HistoricoProfissional.cs
+++ UniversityProject/Models/HistoricoProfissional.cs
@@ -10,15 +10,21 @@ namespace UniversityProject.Models
         public int HistoricoProfissinalID { get; set; }
 
         [Display(Name = "Cargo")]
+        [Required(ErrorMessage = "Obrigatorio informar o Cargo")]
         public string Cargo { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar o Empregador")]
         [Display(Name = "Empregador")]
         public string Empregador { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar o Cidade")]
         [Display(Name = "Cidade")]
         public string Cidade { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar o Estado")]
         [Display(Name = "Estado")]
         public string Estado { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar a Data de Inicio")]
         [Display(Name = "Data de inicio")]
         public DateTime DataInicio { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar a Data de Termino")]
         [Display(Name = "Data de termino")]
         public DateTime DataTermino { get; set; }
 
